211b5c3e6af5b9e809f9580d220993807be8961e
Appanna <appannabm26@gmail.com>
2026-08-07 20:52:12 +0530

    Customized the base template

--- club/routes.py
+++ club/routes.py
@@ -1,5 +1,10 @@
 from club import app
+from datetime import datetime
 from flask import render_template
+
+@app.context_processor
+def inject_globals():
+    return {'current_year':datetime.now().year}
 @app.route('/')
 @app.route('/home')
 def home():
